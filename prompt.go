b73c0692274b83e2093d29dd42c4e107c37c6077
JohnnyDevOps <john@xonk.org>
2016-03-22 00:01:49 -0600

    PromptConfirm() function, and stripchars() function

--- prompt.go
+++ prompt.go
@@ -171,11 +171,20 @@ func WhichFile() {
 		}
 		switch choice {
 		case "n", "N":
+			NewCharacter()
+			Continue()
 			return
 		case "b", "B":
 			return
 		}
 	}
+}
+
+func NewCharacter() {
+	newName := PromptConfirm("What shall your name be? ")
+
+	fmt.Println("Then we shall call you,", newName)
+
 }
 
 func SaveMgr(c *Agent) {
@@ -396,4 +405,54 @@ func Banner() {
 			`   \_| \___/ \___/\____/ \__,_|_|  \____/ \__,_/___|   ` + "\n" +
 			`                                                       `))
 	WhichFile()
+}
+
+//PromptConfirm returns a user entered string with confirmation from
+// the user.  e.g.
+//answer := PromptConfirm("What's your name? ")
+//fmt.Println("Your name is:", answer)
+func PromptConfirm(question string) string {
+	// assign var input outside of loop so that it's in scope for return
+	var response string
+Ask:
+	for {
+		fmt.Printf("%s", question)
+
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		response = scanner.Text()
+
+		// Remove all non alpha characters, including spaces
+		response = stripchars(response, " 1234567890,>?<|/{}[]=+-_*&^%$#@!/(/)\\")
+		// don't allow empty
+		if response == "" {
+			continue
+		}
+
+	Confirm:
+		for {
+			fmt.Printf("You choose: %s. confirm y/n? > ", response)
+
+			reader := bufio.NewReader(os.Stdin)
+			input, _ := reader.ReadString('\n')
+			choice := string([]byte(input)[0])
+
+			switch choice {
+			case "y", "Y":
+				break Ask
+			case "n", "N":
+				break Confirm
+			}
+		}
+	}
+	return response
+}
+
+func stripchars(str, chr string) string {
+	return strings.Map(func(r rune) rune {
+		if strings.IndexRune(chr, r) < 0 {
+			return r
+		}
+		return -1
+	}, str)
 }
